d0159fd7b350831071b5d694fd168616615fbaf5
Chris Judevine <138900704+chris-judevine@users.noreply.github.com>
2026-05-14 11:00:06 -0700

    Add files via upload

---
+++ dep/handler2.o.d
@@ -0,0 +1,50 @@
+handler2.o: handler2.c \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iotn45.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iotnx5.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h \
+  c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stdbool.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stddef.h utility.h \
+  ps2device.h ps2host.h
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iotn45.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iotnx5.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h:
+
+c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stdbool.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stddef.h:
+
+utility.h:
+
+ps2device.h:
+
+ps2host.h:
---
+++ dep/ps2device.o.d
@@ -0,0 +1,45 @@
+ps2device.o: ps2device.c \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iotn45.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iotnx5.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stdbool.h utility.h \
+  ps2device.h
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iotn45.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iotnx5.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stdbool.h:
+
+utility.h:
+
+ps2device.h:
